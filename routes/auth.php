03b7564951a9e1ae0f22a6eb5b00ce935857197b
simoncct1 <simoncouclet@gmail.com>
2024-04-30 20:00:06 +0200

    okok pp + logout + ...

--- routes/auth.php
+++ routes/auth.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::post('/register', [RegisteredUserController::class, 'store'])
                 ->middleware('guest')
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactFormController;
-
+use App\Http\Controllers\ProfileController;
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
